9223319d05cd82da7e1e10f2cad16f2e94d241ab
Gergely Nemeth <ngergo100@gmail.com>
2019-11-18 23:28:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Version bumper for iOS projects
+
+**Please use this action with secrets only!**
+
+[How to add a secrets?](https://help.github.com/en/actions/automating-your-workflow-with-github-actions/creating-and-using-encrypted-secrets)
+
+## Inputs
+
+## Example usage
---
+++ index.js
@@ -0,0 +1,79 @@
+const core = require('@actions/core');
+const shell = require(`shelljs`);
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const axios = require('axios');
+
+function getSettingsFor(targetName, settingId) {
+  // TODO: Check this line
+  const buildSettings = shell.exec(`xcodebuild -target ${targetName} -showBuildSettings`, { silent: true });
+  // TODO: Check this line
+  const bundleIds =  buildSettings.split(`\n`).filter(function(item) {
+    return item.includes(settingId)
+  });
+  if (bundleIds.length == 1) {
+    const bundleId = bundleIds[0].replace(`${settingId} = `,"");
+    return bundleId.replace(/\s/g, "");;
+  } else {
+    throw `Could not find the Bundle ID of the given target ${targetName}`;
+  }
+}
+
+function getToken(issuerID, minute, privateKey, keyId) {
+  const payload = { 
+    exp: Math.floor(Date.now() / 1000) + (minute * 60),
+    aud: "appstoreconnect-v1",
+    iss: issuerID
+  };
+  const options = {
+    algorithm: "ES256",
+    header: { 
+      kid: keyId
+    }
+  }
+  return jwt.sign(payload, privateKey, options);
+}
+
+async function get(url, params, token, method = "GET") {
+  const options = {
+    url: url,
+    method: method,
+    headers: {
+      'Authorization': `Bearer ${token}`
+    },
+    params: params
+  }
+
+  const response = await axios.request(options);
+  return response.data;
+}
+
+async function bumpVersion() {
+  try {
+    const appstoreConnectPrivateKey = core.getInput("appstoreConnectPrivateKey");
+    const keyID = core.getInput("keyID");
+    const issuerID = core.getInput("issuerID");
+    const targetName = core.getInput("targetName");
+    const bundleIdIdentifier = "PRODUCT_BUNDLE_IDENTIFIER";
+    const bundleId = getSettingsFor(targetName, bundleIdIdentifier);
+    const token = getToken(issuerID, 2, Buffer.from(appstoreConnectPrivateKey, "utf8"), keyID);
+    const appResponse = await get("https://api.appstoreconnect.apple.com/v1/apps", { "filter[bundleId]" : bundleId }, token);
+    const appId = appResponse.data[0].id;
+    if (appId) {
+      const builds = await get("https://api.appstoreconnect.apple.com/v1/builds", { "filter[app]": appId, limit: 1, sort: "-version" }, token);
+      const currentBuildNumber = builds.data[0].attributes.version;
+      if (currentBuildNumber) {
+        const nextBuildNumber = Number(currentBuildNumber) + 1;
+        shell.exec(`xcrun agvtool new-version ${nextBuildNumber}`);
+      } else {
+        throw `Could not find the Version Number for ${appId}`;  
+      }
+    } else {
+      throw `Could not find the App ID for ${bundleId}`;
+    }
+  } catch (error) {
+    core.setFailed(error);
+  }
+}
+
+bumpVersion();
